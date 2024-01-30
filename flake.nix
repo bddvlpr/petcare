476b95546b33fee82286e19f7f77d0a2c4e00200
Luna Simons <luna@bddvlpr.com>
2024-01-30 22:59:47 +0100

    feat: prisma, docker and tailwindcss

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            prisma-engines
+            nodePackages.prisma
+            nodePackages.pnpm
+          ];
+
+          shellHook = with pkgs; ''
+            export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
+            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+          '';
+        };
+      };
+    };
+}
