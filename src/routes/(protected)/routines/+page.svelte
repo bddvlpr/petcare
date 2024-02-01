480da4eb1afd6c19c9c058bff48b508e775eeb80
Luna Simons <luna@bddvlpr.com>
2024-02-01 21:37:23 +0100

    fix: routines to current

--- src/routes/(protected)/routines/+page.svelte
+++ src/routes/(protected)/routines/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  import { page } from '$app/stores';
   import type { PageData } from './$types';
 
   export let data: PageData;
-
-  $: pages = Math.ceil(data.count / 10);
 </script>
 
 {#each data.routines as routine}
