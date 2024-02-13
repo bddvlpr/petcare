9145605bb0b24eb4d859f7692f8156698433928a
Luna Simons <luna@bddvlpr.com>
2024-02-13 16:03:48 +0100

    feat: containerization

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:20 AS build-env
+
+ENV PNPM_HOME="/pnpm"
+ENV PATH="$PNPM_HOME:$PATH"
+
+RUN corepack enable
+
+WORKDIR /app
+COPY . .
+
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
+RUN pnpm run build
+
+FROM node:20-slim AS runtime-env
+
+WORKDIR /app
+COPY --from=build-env /app/package.json .
+COPY --from=build-env /app/build .
+
+EXPOSE 3000
+
+CMD ["node", "index.js"]
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,13 @@
-import { AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, AUTH0_ISSUER } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Auth0 from '@auth/sveltekit/providers/auth0';
 
 export const handle = SvelteKitAuth({
   providers: [
     Auth0({
-      clientId: AUTH0_CLIENT_ID,
-      clientSecret: AUTH0_CLIENT_SECRET,
-      issuer: AUTH0_ISSUER,
+      clientId: env.AUTH0_CLIENT_ID,
+      clientSecret: env.AUTH0_CLIENT_SECRET,
+      issuer: env.AUTH0_ISSUER,
       authorization: {
         params: {
           prompt: 'login'
