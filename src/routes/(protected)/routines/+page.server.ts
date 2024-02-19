9f23c6a3cb5cf2df7e6eaebc3ab99864a830543f
Luna Simons <luna@bddvlpr.com>
2024-02-19 14:11:44 +0100

    style: oopsie

--- src/routes/(protected)/routines/+page.server.ts
+++ src/routes/(protected)/routines/+page.server.ts
@@ -2,7 +2,6 @@ import { getAllRoutines, getDueRoutines } from '$lib/server/services/routines';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-  console.log(await getAllRoutines());
   return {
     routines: await getAllRoutines(),
     dueRoutines: await getDueRoutines()
